a18766e2b2e5bce98a1883c27dd848feab3b01d6
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-08 00:36:49 -0700

    fix

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -13,7 +13,7 @@ const Rightbar = ({ user }) => {
   const [friends, setFriends] = useState([]);
   const { user: currentUser, dispatch } = useContext(AuthContext);
   const [followed, setFollowed] = useState(
-    currentUser.followings.includes(user?.id)
+    currentUser.followings.includes(user?._id)
   );
 
   useEffect(() => {
