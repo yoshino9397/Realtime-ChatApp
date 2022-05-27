f657b040dd30448fa006a5526452a7ff7104da77
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-26 20:34:01 -0700

    some update

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -13,7 +13,6 @@ export const loginCall = async (userCredential, dispatch) => {
 export const logoutCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGOUT_START" });
   try {
-    // const res = await axios.post("auth/login", userCredential);
     dispatch({ type: "LOGOUT_SUCCESS", payload: userCredential });
   } catch (err) {
     dispatch({ type: "LOGOUT_FAILURE", payload: err });
